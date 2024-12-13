aab3aaa089f75404f07dd790aa193bb430a5582a
KANAjetzt <41547570+KANAjetzt@users.noreply.github.com>
2024-12-13 23:29:00 +0100

    fix: :bug: fix `\` breaking function declaration (#486)
    * fix: :bug: fix `\` breaking function declaration
    
    closes #472
    
    * fix: :bug: only replace `\\n` not all `\`

--- addons/mod_loader/internal/mod_hook_preprocessor.gd
+++ addons/mod_loader/internal/mod_hook_preprocessor.gd
@@ -248,6 +248,7 @@ static func get_function_parameters(method_name: String, text: String, is_static
 	# Clean whitespace characters (spaces, newlines, tabs)
 	param_string = param_string.strip_edges()\
 		.replace(" ", "")\
+		.replace("\\\n", "")\
 		.replace("\n", "")\
 		.replace("\t", "")\
 		.replace(",", ", ")\
